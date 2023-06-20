13448e683d039b381b5e4ee2b872d7ff8323cf96
Dillon McElhinney <dillon.mce@gmail.com>
2023-06-20 11:53:24 -0600

    Rearrange and add docs for fetching activity

--- Interview/Core/Network.swift
+++ Interview/Core/Network.swift
@@ -9,18 +9,21 @@ import Foundation
 import Alamofire
 
 class Network {
-    func getContent(completion: @escaping (Result<Month, Error>) -> Void) {
+
+    /// Fetches the user's activity. The resulting array of Days can be assumed to be contiguous and
+    /// in ascending order by date
+    func getActivity(completion: @escaping (Result<[Day], Error>) -> Void) {
         AF.request("https://hallow.com/interview/activity.json", method: .get)
             .responseDecodable(of: [Day].self, decoder: JSONDecoder()) { response in
-                if let error = response.error {
-                    completion(.failure(error))
-                }
-
-                // Data is already decoded as array of `Day`s
-                print(response.result)
-
                 // TODO: Finish creating models
-                // TODO: Get days that are in first month and pass to completion
+                completion(response.result)
+        }
+    }
+
+    func getContent(completion: @escaping (Result<Month, Error>) -> Void) {
+        getActivity { result in
+            print(result)
+            // TODO: Get days that are in first month and pass to completion
         }
     }
 
